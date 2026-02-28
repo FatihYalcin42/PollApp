641db29581a03840198c8831559f602f0fd1d62e
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-28 10:25:49 +0100

    feat(single-survey): add new single-survey route with purple logo and initial muted canvas layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CreateSurveyPage } from './pages/create-survey/create-survey-page';
 import { HomePage } from './pages/home/home-page';
+import { SingleSurveyPage } from './pages/single-survey/single-survey-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'create-survey', component: CreateSurveyPage },
+  { path: 'single-survey', component: SingleSurveyPage },
   { path: '**', redirectTo: '' },
 ];
---
+++ src/app/pages/single-survey/single-survey-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-survey-page',
+  templateUrl: './single-survey-page.html',
+  styleUrl: './single-survey-page.scss',
+})
+export class SingleSurveyPage {}
