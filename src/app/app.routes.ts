d013ae745188d77b9da924c552521e390dac9eea
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-28 08:38:06 +0100

    feat: add routed create-survey page with fixed workspace background container

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CreateSurveyPage } from './pages/create-survey/create-survey-page';
+import { HomePage } from './pages/home/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'create-survey', component: CreateSurveyPage },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/pages/create-survey/create-survey-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-survey-page',
+  templateUrl: './create-survey-page.html',
+  styleUrl: './create-survey-page.scss',
+})
+export class CreateSurveyPage {}
