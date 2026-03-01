effd5371e4f2026b781ac04dd7206212e075c44c
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-03-01 10:58:30 +0100

    feat(create-survey): add dynamic answer inputs (max 6) with scrollable answers section

--- src/app/pages/create-survey/create-survey-page.ts
+++ src/app/pages/create-survey/create-survey-page.ts
@@ -9,8 +9,23 @@ import { RouterLink } from '@angular/router';
 })
 export class CreateSurveyPage {
   protected isCategoryDropdownOpen = false;
+  protected readonly maxAnswerFields = 6;
+  protected answerFieldIndexes = [0, 1];
 
   protected toggleCategoryDropdown(): void {
     this.isCategoryDropdownOpen = !this.isCategoryDropdownOpen;
+  }
+
+  protected addAnswerField(): void {
+    if (this.answerFieldIndexes.length >= this.maxAnswerFields) {
+      return;
+    }
+
+    const nextIndex = this.answerFieldIndexes.length;
+    this.answerFieldIndexes = [...this.answerFieldIndexes, nextIndex];
+  }
+
+  protected getAnswerLabel(index: number): string {
+    return String.fromCharCode(65 + index);
   }
 }
